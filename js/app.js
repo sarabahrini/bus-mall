101b7115747dd2ef7166e4dcfb6f0f8a1460c036
Jane Smith <example@email.com>
2018-05-16 16:13:06 -0700

    almost there

--- js/app.js
+++ js/app.js
@@ -9,73 +9,65 @@ var object2 = [];
 var object3 = [];
 
 //linking js to html
-var imgEl1 = document.getElementById('image-1');
-var imgEl2 = document.getElementById('image-2');
-var imgEl3 = document.getElementById('image-3');
+var imgEl1 = document.getElementById('0');
+var imgEl2 = document.getElementById('1');
+var imgEl3 = document.getElementById('2');
 var sectionEl = document.getElementById('clickerBox');
 var resultList = document.getElementById('voteResult');
 
+// setting up a counter for number of clicks for each picture
 var pic1Index = 0;
-var pic2Index = 0;
-var pic3Index = 0;
+var pic2Index = 1;
+var pic3Index = 2;
 
 var checkTotalClicks = 0;
 
 // global object with general this properties
 function Picture(src, name) {
-    this.picName = name;
+    this.name = name;
     this.url = src;
-
     this.clicked = 0;
     allPictures.push(this);
 }
 
 
-//adding an eventListener
-imgEl1.addEventListener('click', wasClicked1)
-imgEl2.addEventListener('click', wasClicked2)
-imgEl3.addEventListener('click', wasClicked3)
+//Event Listeners
+//this is to make sure we are only counting the clicks ON THE PICTURES ONLY
+sectionEl.addEventListener('click', sectionCallback);
 
-//eventHandler 
-function wasClicked1() { // once we reach the total 25 clicks show the result
 
-    object1.clicked++;
-    checkTotalClicks++;
-    if (checkTotalClicks = 25) {
-        showResults();
+function sectionCallback(event) {
+    totalClicks();
+console.log(allPictures[event.target.id]);
+    if (event.target.id) {
+        console.log(allPictures);
+        checkTotalClicks++;
+        allPictures[event.target.id].clicked++;
+        // allPictures[event.target.id].clicked++;
+
+        chooseNewImg();
+    } else {
+        alert('click on an image');
     }
-
-    chooseNewImg();
-
-};
-
-
-//eventHandler 
-function wasClicked2() { // once we reach the total 25 clicks show the result
-
-    object2.clicked++;
-    checkTotalClicks++;
-    if (checkTotalClicks = 25) {
-        showResults();
-    }
-
-    chooseNewImg();
-};
-
-
-
-//eventHandler 
-function wasClicked3() { // once we reach the total 25 clicks show the result
-
-    object3.clicked++;
-    checkTotalClicks++;
-if(checkTotalClicks=25){
-    showResults(); 
 }
 
-chooseNewImg();
+//to create result list in our page we do this 
+function renderResults() {
+    for (var i in allPictures) {
+        var newLiEl = document.createElement('li');
+        newLiEl.textContent = allPictures[i].name + ' clicked : ' + allPictures[i].clicked + ' Times';
+        resultList.appendChild(newLiEl);
+    }
+}
 
-};
+function totalClicks() {
+    if (checkTotalClicks === 25) {
+
+        renderResults();
+
+        sectionEl.removeEventListener('click', sectionCallback);
+    }
+}
 
 
 new Picture("img/bag.jpg", "bag");
@@ -101,40 +93,46 @@ new Picture("img/wine-glass.jpg", "wine-glass");
 
 
 
+// function chooseNewImg() {  // pick a random picture from allPictures array // avoid duplication
+
+
 
 function chooseNewImg() {  // pick a random picture from allPictures array // avoid duplication
 
-    do {
-        var index = Math.floor(Math.random() * 20);
-        object1 = allPictures[index];
-    }
-    while (cantBeThis.includes(object1))
-    cantBeThis.push(object1);
-    imgEl1.src = object1.url;
+    do{
+        pic1Index = Math.floor(Math.random() * allPictures.length);
+      } while (cantBeThis.includes(pic1Index));
+      cantBeThis.push(pic1Index);
+    
+      do{
+        pic2Index = Math.floor(Math.random() * allPictures.length);
+      } while (cantBeThis.includes(pic2Index));
+      cantBeThis.push(pic2Index);
+    
+      do {
+        pic3Index = Math.floor(Math.random() * allPictures.length);
+      } while (cantBeThis.includes(pic3Index));
 
 
-    do {
-        var index = Math.floor(Math.random() * 20);
-        object2 = allPictures[index];
-    }
-    while (cantBeThis.includes(object2))
-    cantBeThis.push(object2);
-    imgEl2.src = object2.url;
+    // object1 = allPictures[pic1Index];
+    imgEl1.src = allPictures[pic1Index].url;
+    imgEl1.id = pic1Index
 
+    
+    // object2 = allPictures[pic2Index];
+    imgEl2.src = allPictures[pic2Index].url;
+    imgEl2.id = pic2Index;
 
-    do {
-        var index = Math.floor(Math.random() * 20);
-        object3 = allPictures[index];
-    }
-    while (cantBeThis.includes(object3))
-    cantBeThis.push(object3);
-    imgEl3.src = object3.url;
+    
+    // object3 = allPictures[pic3Index];
+    imgEl3.src = allPictures[pic3Index].url;
+    imgEl3.id = pic3Index;
 }
 
-chooseNewImg();
+
+// chooseNewImg();
+
+// function showResults() {
 
 
-function showResults() {
-
-
-};
+// };
